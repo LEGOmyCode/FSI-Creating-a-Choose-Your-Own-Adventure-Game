8704501ef82d4355af8531a04e029ad816717f93
Raymond Mojica <rm@M12020.local>
2022-03-29 17:17:10 -0400

    Working on Adventure Game

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-//Your Code here
+window.prompt('Do you head left or right?')// Let's start by asking the user a question
+let answer = window.prompt('Do you head left or right?')// We can get the answer the user types in by assigning a variable
+let firstAnswer = window.prompt('Do you head left or right?')//
+console.log(firstAnswer)
